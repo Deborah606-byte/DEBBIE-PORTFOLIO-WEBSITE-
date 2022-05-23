deeefe45fe091566ab024e2f1f89c36320d29170
Deborah606-byte <paintsildeborah248@gmail.com>
2022-05-23 17:39:15 +0000

    add javascript folder

---
+++ JAVASCRIPT/about.js
@@ -0,0 +1,13 @@
+function validateForm(){
+    var input1 = document.getElementById('text').value;
+    var input2 = document.getElementById('email').value;
+
+    if ((input1=="") ) 
+     {
+         alert( "You must type in your name");
+     }
+    if ((emial=="") )
+    {
+       alert("You must type in your Email");
+    }
+}
